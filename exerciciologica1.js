6f2412bb7d89f837878d8670b8842a6aa420d276
LaercioMoreira96 <contato.laerciomoreira@gmail.com>
2025-01-08 14:20:12 -0300

    alterado sintaxe do if

--- exerciciologica1.js
+++ exerciciologica1.js
@@ -1,11 +1,8 @@
 // Escrever função que receba dois numeros e retorne o maior entre eles
 
 function recebeNumero(num1, num2){
-    if (num1 > num2){
-        return num1
-    } else {
-        return num2
-    }
+    if (num1 > num2) return num1;
+    return num2;
 }
 
 console.log(recebeNumero(5,4))
