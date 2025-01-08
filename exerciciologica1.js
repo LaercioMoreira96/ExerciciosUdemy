4d174c05f7621a233c0888467648bc156b0336fe
LaercioMoreira96 <contato.laerciomoreira@gmail.com>
2025-01-08 14:15:41 -0300

    commitexercicios

---
+++ exerciciologica1.js
@@ -0,0 +1,11 @@
+// Escrever função que receba dois numeros e retorne o maior entre eles
+
+function recebeNumero(num1, num2){
+    if (num1 > num2){
+        return num1
+    } else {
+        return num2
+    }
+}
+
+console.log(recebeNumero(5,4))
