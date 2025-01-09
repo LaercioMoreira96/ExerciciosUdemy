1ebac44b7bae3959002ea09afd40b87c8f7fdbba
LaercioMoreira96 <contato.laerciomoreira@gmail.com>
2025-01-09 14:19:00 -0300

    altrada para arrow function

--- exerciciologica2.js
+++ exerciciologica2.js
@@ -1,8 +1,6 @@
 // Escrever função que recebe 2 argumentos (largura e altura) de uma imagem. Retorne true se a imagem estiver no modo paisagem
 
-function ePaisagem(largura, altura) {
-    return largura > altura ? "True":"False"
+const ePaisagem = (largura, altura) => largura > altura;
     
-}
 
-console.log(ePaisagem(1920, 1080))
+console.log(ePaisagem(1920, 1080));
