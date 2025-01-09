1ad657f053010d2279b759f73606a113353bfd52
LaercioMoreira96 <contato.laerciomoreira@gmail.com>
2025-01-09 14:00:23 -0300

    exercicio2add

---
+++ exerciciologica2.js
@@ -0,0 +1,8 @@
+// Escrever função que recebe 2 argumentos (largura e altura) de uma imagem. Retorne true se a imagem estiver no modo paisagem
+
+function ePaisagem(largura, altura) {
+    return largura > altura ? "True":"False"
+    
+}
+
+console.log(ePaisagem(1920, 1080))
